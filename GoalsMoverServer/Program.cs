ca5503af6b7e0b8e235e18dbc08a03fe9b602fa9
Maksym Yaremko <maksym.yaremko@gmail.com>
2020-03-30 00:13:09 +0300

    configure basic serilog logging for log in app

--- GoalsMoverServer/Program.cs
+++ GoalsMoverServer/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
 
 namespace GoalsMover
 {
@@ -7,14 +10,62 @@ namespace GoalsMover
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Console.WriteLine("Golaer app started");
+
+            var configuration = ReadConfiguration(Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT"));
+
+            InitializeLogger(configuration);
+
+            try
+            {
+                Log.Information("Starting the host");
+
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly.");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+                .UseSerilog();
+        }
+
+        private static IConfiguration ReadConfiguration(string environment)
+        {
+            var config = new ConfigurationBuilder();
+
+            config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            return config.Build();
+        }
+
+        private static void InitializeLogger(IConfiguration configuration)
+        {
+            try
+            {
+                Log.Logger = new LoggerConfiguration()
+                    .ReadFrom.Configuration(configuration)
+                    .CreateLogger();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Failed to initialize the logger. Message: {e.Message}. Stack: {e.StackTrace}");
+                throw;
+            }
+        }
     }
 }
